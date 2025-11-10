4dd815db5a82d67939520dc926c4e5a0742953e7
Danil Anikin <mart4r.clearlist@gmail.com>
2025-11-11 09:59:53 +1000

    Initial Commit

---
+++ DeviceScanApp/DeviceScanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceScanAppApp.swift
+//  DeviceScanApp
+//
+//  Created by Данил Аникин on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeviceScanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
